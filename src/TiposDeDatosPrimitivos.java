4c90a60a3902a31296c98e900974cd4fbc28f632
Rodrigo Vera <prodoctionsmr@gmail.com>
2022-07-20 14:30:54 -0500

    Commit Inicial

---
+++ src/TiposDeDatosPrimitivos.java
@@ -0,0 +1,8 @@
+public class TiposDeDatosPrimitivos {
+    public static void main(String[] args) {
+        byte valorMinimo = Byte.MIN_VALUE;
+        byte valorMaximo = Byte.MAX_VALUE;
+
+        System.out.println("Valor mínimo de byte: " + valorMinimo + " y valor máximo de byte: " + valorMaximo);
+    }
+}
